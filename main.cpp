f22204d115a515a162cf5b4f8a8020e3d0018875
BupTy03 <41854435+BupTy03@users.noreply.github.com>
2018-08-08 15:02:10 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 int main()
 {
-	SMatrix<int, 4, 4> mtx = { 	3, -3, -5, 	8,
-							   -3,  2,  4, -6,
-							    2, -5, -7,  5,
-							   -4,  3,  5, -6 };
+	SMatrix<int, 4, 4> mtx = { 3, -3, -5, 8,
+				  -3,  2,  4, -6,
+				   2, -5, -7,  5,
+				  -4,  3,  5, -6 };
 	Determinant<int> det{mtx};
 
 	cout << det.det() << endl;
